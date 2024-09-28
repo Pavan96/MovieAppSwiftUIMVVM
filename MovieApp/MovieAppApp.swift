e2771d13c6ab8539e68a2bfbc0ecbb449fa09282
Pavan <pavan.more1@gmail.com>
2024-09-28 09:43:19 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Pavan More on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
